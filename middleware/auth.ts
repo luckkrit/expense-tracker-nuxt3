8037e4e62038eebb551ffa752d324515ba4c1fc9
Krit Chomaitong <luckkrit@hotmail.com>
2024-05-07 06:45:42 +0000

    update comment

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,9 +1,10 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const user = useCookie('user')
+    // if login then access backend
     if (from.path === '/' && to.path === '/' && (user.value !== null && user.value !== undefined)) {
         return navigateTo('/dashboard')
     }
-    // if not login then cannot access backend
+    // if not login then redirect to index
     if ((from.path !== '/' && to.path !== '/') && (user.value === null || user.value === undefined)) {
         return navigateTo('/')
     }
